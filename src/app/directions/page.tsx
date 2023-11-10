de40343afdca34a8aa0cddd48e3d340e248ef324
Leigh Halliday <leighhalliday@gmail.com>
2023-11-10 14:17:32 -0500

    Finishing up directions demo

--- src/app/directions/page.tsx
+++ src/app/directions/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
 import {
   APIProvider,
   Map,
-  useDirectionsService,
+  useMapsLibrary,
+  useMap,
 } from "@vis.gl/react-google-maps";
 
 export default function Intro() {
   const position = { lat: 43.6532, lng: -79.3832 };
+
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
@@ -27,68 +28,65 @@ export default function Intro() {
 }
 
 function Directions() {
-  const { renderRoute, setRenderedRouteIndex } = useDirectionsService({
-    renderOnMap: true,
-  });
+  const map = useMap();
+  const routesLibrary = useMapsLibrary("routes");
+  const [directionsService, setDirectionsService] =
+    useState<google.maps.DirectionsService>();
+  const [directionsRenderer, setDirectionsRenderer] =
+    useState<google.maps.DirectionsRenderer>();
   const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
   const [routeIndex, setRouteIndex] = useState(0);
   const selected = routes[routeIndex];
   const leg = selected?.legs[0];
 
   useEffect(() => {
-    if (!renderRoute) return;
+    if (!routesLibrary || !map) return;
+    setDirectionsService(new routesLibrary.DirectionsService());
+    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
+  }, [routesLibrary, map]);
 
-    renderRoute({
-      origin: "100 Front St, Toronto, ON",
-      destination: "500 College St, Toronto, ON",
-      travelMode: google.maps.TravelMode.DRIVING,
-      provideRouteAlternatives: true,
-    }).then((response) => {
-      setRoutes(response.routes);
-    });
-  }, []);
+  useEffect(() => {
+    if (!directionsService || !directionsRenderer) return;
+
+    directionsService
+      .route({
+        origin: "100 Front St, Toronto ON",
+        destination: "500 College St, Toronto ON",
+        travelMode: google.maps.TravelMode.DRIVING,
+        provideRouteAlternatives: true,
+      })
+      .then((response) => {
+        directionsRenderer.setDirections(response);
+        setRoutes(response.routes);
+      });
+  }, [directionsService, directionsRenderer]);
+
+  useEffect(() => {
+    if (!directionsRenderer) return;
+    directionsRenderer.setRouteIndex(routeIndex);
+  }, [routeIndex, directionsRenderer]);
+
+  if (!leg) return null;
 
   return (
-    <>
-      {leg && (
-        <div className="directions">
-          <h2>{selected.summary}</h2>
-          <p>
-            {leg.start_address.split(",")[0]} to {leg.end_address.split(",")[0]}
-          </p>
-          <p>
-            <small>Distance: {leg.distance?.text}</small>
-          </p>
-          <p>
-            <small>Duration: {leg.duration?.text}</small>
-          </p>
+    <div className="directions">
+      <h2>{selected.summary}</h2>
+      <p>
+        {leg.start_address.split(",")[0]} to {leg.end_address.split(",")[0]}
+      </p>
+      <p>Distance: {leg.distance?.text}</p>
+      <p>Duration: {leg.duration?.text}</p>
 
-          {routes.length > 0 && (
-            <>
-              <h2>Other Routes</h2>
-              <ul>
-                {routes.map((route, index) => (
-                  <li key={route.summary}>
-                    <button
-                      className="btn-route"
-                      onClick={() => {
-                        setRouteIndex(index);
-                        setRenderedRouteIndex?.(index);
-                      }}
-                    >
-                      {index === routeIndex ? (
-                        <strong>{route.summary}</strong>
-                      ) : (
-                        route.summary
-                      )}
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-        </div>
-      )}
-    </>
+      <h2>Other Routes</h2>
+      <ul>
+        {routes.map((route, index) => (
+          <li key={route.summary}>
+            <button onClick={() => setRouteIndex(index)}>
+              {route.summary}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
