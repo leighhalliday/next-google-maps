f28143625062ca31604d271cd5b3bb399dc3d099
Leigh Halliday <leighhalliday@gmail.com>
2023-11-10 15:02:47 -0500

    Finished markers

--- src/app/markers/page.tsx
+++ src/app/markers/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
-import { Markers } from "../../components/markers";
+import {
+  APIProvider,
+  Map,
+  useMap,
+  AdvancedMarker,
+} from "@vis.gl/react-google-maps";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import type { Marker } from "@googlemaps/markerclusterer";
+import React, { useEffect, useState, useRef } from "react";
 import trees from "../../data/trees";
+// [{ name: "Oak, English", lat: 43.64, lng: -79.41, key: "ABCD" }]
 
 export default function Intro() {
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
         <Map
-          center={{ lat: trees[0].lat, lng: trees[0].lng }}
+          center={{ lat: 43.64, lng: -79.41 }}
           zoom={10}
           mapId={process.env.NEXT_PUBLIC_MAP_ID}
         >
@@ -19,3 +27,53 @@ export default function Intro() {
     </div>
   );
 }
+
+type Point = google.maps.LatLngLiteral & { key: string };
+type Props = { points: Point[] };
+
+const Markers = ({ points }: Props) => {
+  const map = useMap();
+  const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
+  const clusterer = useRef<MarkerClusterer | null>(null);
+
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map });
+    }
+  }, [map]);
+
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+
+  const setMarkerRef = (marker: Marker | null, key: string) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers((prev) => {
+      if (marker) {
+        return { ...prev, [key]: marker };
+      } else {
+        const newMarkers = { ...prev };
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
+  return (
+    <>
+      {points.map((point) => (
+        <AdvancedMarker
+          position={point}
+          key={point.key}
+          ref={(marker) => setMarkerRef(marker, point.key)}
+        >
+          <span style={{ fontSize: "2rem" }}>🌳</span>
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
